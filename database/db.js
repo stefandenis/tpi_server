6a18c4e8ece1b73a1fb756a162b8062b9530a989
aphosura4ever <stefan.denis03@yahoo.com>
2020-01-11 12:04:28 +0200

    hosted db

--- database/db.js
+++ database/db.js
@@ -1,17 +1,17 @@
 const mysql = require("mysql");
 
-// const connection = mysql.createConnection({
-//     host: "remotemysql.com",
-//     user: "3o4Qp0UaYn",
-//     password: "tDsICUNOEz",
-//     database: "3o4Qp0UaYn"
-//   });
-  const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "qwertyhnjkl",
-    database: "login"
+const connection = mysql.createConnection({
+    host: "remotemysql.com",
+    user: "3o4Qp0UaYn",
+    password: "tDsICUNOEz",
+    database: "3o4Qp0UaYn"
   });
+  // const connection = mysql.createConnection({
+  //   host: "localhost",
+  //   user: "root",
+  //   password: "qwertyhnjkl",
+  //   database: "login"
+  // });
 
   
   connection.connect(err => {
